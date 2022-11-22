93db79583e49a126e5cbaefa49152e52f9791f4c
Jyoti Prakash Panda <jppanda12345@gmail.com>
2022-11-22 10:13:18 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Timer from './Timer';
+
+function App() {
+  return (
+    <div className="App" styles={appStyles}>
+      <div className="app-container">
+        <h1>Countdown Timer</h1> 
+        <Timer />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+const appStyles = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center'
+}
---
+++ src/TimeForm.js
@@ -0,0 +1,59 @@
+import React, { Component, createRef } from 'react';
+
+class TimeForm extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      hours: props.hours,
+      minutes: props.minutes,
+      seconds: props.seconds
+    }
+    this.formRef = createRef();
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const {hours, minutes, seconds} = this.state;
+    this.props.callback(hours, minutes, seconds);
+    this.formRef.current.style.display = 'none';
+  }
+
+  handleChange = (e) => {
+    const {name, value} = e.target;
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleClose = (e) => {
+    debugger
+    this.formRef.current.style.display = 'none';
+  }
+
+  render() {
+    return (
+      <div className='modal' id='time-form' ref={this.formRef}>
+        <form className='modal-content' onSubmit={this.handleSubmit}>
+          <div className='close-btn' onClick={this.handleClose}>X</div>
+
+          <label>Hours</label>
+          <input onChange={this.handleChange} name='hours' type='text' value={this.state.hours} />
+          <br />
+
+          <label>Minutes</label>
+          <input onChange={this.handleChange} name='minutes' type='text' value={this.state.minutes} />
+          <br />
+
+          <label>Seconds</label>
+          <input onChange={this.handleChange} name='seconds' type='text' value={this.state.seconds} />
+          <br />
+
+          <button type='submit'>Set Time</button>
+        </form>
+      </div>
+    );
+  }
+
+}
+
+export default TimeForm;
